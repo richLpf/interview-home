51f6259cd5dae1f35798f88414f434c711e33feb
richLpf <lpengfei66@163.com>
2023-11-12 21:27:42 +0800

    feat: passage

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,20 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
+  logo: <span>费曼学习</span>,
+  // project: {
+  //   link: 'https://github.com/shuding/nextra-docs-template',
+  // },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '用提问的方式来学习',
   },
+  search: {
+    placeholder: '在这里输入搜索内容'
+  }
 }
 
 export default config
