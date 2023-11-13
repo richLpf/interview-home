f0c21aab4b67b1dc108351d042c957b19e9283c0
richLpf <lpengfei66@163.com>
2023-11-13 23:03:34 +0800

    feat: feiman

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>费曼学习</span>,
+  logo: <div>
+      <img 
+        width="30px" 
+        style={{
+          display: 'inline-block', 
+          marginRight: '4px'
+        }} 
+        src="/images/favicon.ico" alt="logo" 
+      />
+      <span>费曼学习</span>
+    </div>,
   // project: {
   //   link: 'https://github.com/shuding/nextra-docs-template',
   // },
@@ -10,6 +20,14 @@ const config: DocsThemeConfig = {
   //   link: 'https://discord.com',
   // },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  head: () => [
+    <link
+      key="favicon"
+      rel="icon"
+      href="/images/favicon.ico"
+      type="image/x-icon"
+    />
+  ],
   footer: {
     text: '用提问的方式来学习',
   },
